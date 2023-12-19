562bce4784b73d1b3a087c16b46803805c81dd4d
Abhinkjishmon <abhinkjishmon001@gmail.com>
2023-12-19 13:47:58 +0000

    toggle section finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,18 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
+if(navToggle){
+    navToggle.addEventListener('click', ()=>{
+        navMenu.classList.add('show-menu')
+    })
+}
+if(navClose){
+    navClose.addEventListener('click', ()=>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
