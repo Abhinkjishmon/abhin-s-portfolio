09399a8dbbe2aca7cd8755a5828d94dcbcf43780
Abhinkjishmon <abhinkjishmon001@gmail.com>
2023-12-22 22:31:23 +0000

    about section

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,10 @@ const linkAction = () =>{
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+//shadow header
+const shadowHeader = () =>{
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('shadow-header')
+                       : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
