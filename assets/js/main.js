147c2e71120a25c6c6e56672e78b342c80d1f0a8
Abhinkjishmon <abhinkjishmon001@gmail.com>
2024-01-05 22:02:25 +0000

    ald

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
 //show menu
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
-if(navToggle){
-    navToggle.addEventListener('click', ()=>{
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
-if(navClose){
-    navClose.addEventListener('click', ()=>{
+if (navClose) {
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 }
@@ -17,16 +17,16 @@ if(navClose){
 //remove menu mobile
 const navLink = document.querySelectorAll('.nav__link')
 
-const linkAction = () =>{
+const linkAction = () => {
     const navMenu = document.getElementById('nav-menu')
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 //shadow header
-const shadowHeader = () =>{
+const shadowHeader = () => {
     const header = document.getElementById('header')
     this.scrollY >= 50 ? header.classList.add('shadow-header')
-                       : header.classList.remove('shadow-header')
+        : header.classList.remove('shadow-header')
 }
 window.addEventListener('scroll', shadowHeader)
 
@@ -34,29 +34,82 @@ window.addEventListener('scroll', shadowHeader)
 const contactForm = document.getElementById('contact-form'),
     contactMessage = document.getElementById('contact-message')
 
-const sendEmail = (e)=>{
+const sendEmail = (e) => {
     e.preventDefault()
 
-    emailjs.sendForm('service_700sghl','template_nc91n0a','#contact-form','h9bpPWT1eir9vUFAF')
-    .then(() =>{
-        contactMessage.textContent = 'Message sent successfully ✅'
+    emailjs.sendForm('service_700sghl', 'template_nc91n0a', '#contact-form', 'h9bpPWT1eir9vUFAF')
+        .then(() => {
+            contactMessage.textContent = 'Message sent successfully ✅'
 
-        setTimeout(() =>{
-            contactMessage.textContent = ''
-        }, 5000)
+            setTimeout(() => {
+                contactMessage.textContent = ''
+            }, 5000)
 
-        contactForm.reset()
-    }, () =>{
-        contactMessage.textContent = 'Message not sent (service error) ❌'
-    })
+            contactForm.reset()
+        }, () => {
+            contactMessage.textContent = 'Message not sent (service error) ❌'
+        })
 }
 contactForm.addEventListener('submit', sendEmail)
 
 //show scroll up
-const scrollUp = () =>{
+const scrollUp = () => {
     const scrollUp = document.getElementById('scroll-up')
 
     this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
-                        : scrollUp.classList.remove('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
+
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            secionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link')
+        } else {
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'bx ri-sun-line'
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true
+})
+sr.reveal(`.home__perfil, .about__image, .contact__mail`, { origin: 'right' })
+sr.reveal(`.home__name, .home__info,
+            .about__container .section__title-1, .about__info, 
+            .contact__social, .contact__data`, { origin: 'left' })
+sr.reveal(`.services__card, .projects__card`, {interval: 100})
